4bb5f0bb8d666d6463ffbac0cbdee79fc45b206e
Ahmed Hassan <ahmedalhassany1@gmail.com>
2019-10-23 21:31:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+function writeCards(names,event)
+{
+  let arr=[];
+  for(let i=0;i<names.length;i++)
+  {
+    arr[i]="Thank you, "+names[i]+" for the wonderful birthday gift!";
+  }
+  return arr;
+}
+
+//writeCards(["Ada", "Brendan", "Ali"], "birthday");
