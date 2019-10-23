29a70c6ed638128297dad203c2b1e19073d4b714
Ahmed Hassan <ahmedalhassany1@gmail.com>
2019-10-23 21:32:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function writeCards(names,e)
   let arr=[];
   for(let i=0;i<names.length;i++)
   {
-    arr[i]="Thank you, "+names[i]+" for the wonderful"+e +gift!";
+    arr[i]="Thank you, "+names[i]+" for the wonderful"+e +"gift!";
   }
   return arr;
 }
